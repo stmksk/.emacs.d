16088d6aacd9a2e359c6535b90c94eae56ab97a6
KSaotome <kousuke.saotome@liferobotics.jp>
2017-07-27 17:03:06 +0900

    first commit

---
+++ init.el
@@ -0,0 +1,456 @@
+(package-initialize)
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+	("melpa" . "http://melpa.org/packages/")
+	("org" . "http://orgmode.org/elpa/")))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; 初期表示位置、サイズ
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq initial-frame-alist
+      '((left   . 100)			; 位置 (X)
+	(top    .  50)			; 位置 (Y)
+	(width  . 120)			; サイズ(幅)
+	(height .  40)			; サイズ(高さ)
+	))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; スクロール
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; クロールした際のカーソルの移動行数
+(setq scroll-conservatively 1)
+
+;; スクロール開始のマージンの行数
+(setq scroll-margin 10)
+
+
+;; 1 画面スクロール時に重複させる行数
+(setq next-screen-context-lines 10)
+
+;; 1 画面スクロール時にカーソルの画面上の位置をなるべく変えない
+(setq scroll-preserve-screen-position t)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; キー設定
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; BackspaceをC-hに割り当て
+(keyboard-translate ?\C-h ?\C-?)
+
+;;; 文字列置換
+(global-set-key (kbd "C-r") 'replace-regexp)
+
+;;; yes と入力するのは面倒なので y でokにする
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; 行末の空白を表示
+(setq-default show-trailing-whitespace t)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; 検索、置換時の大文字、小文字の区別
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; オプションの "Ignore Case for Search" で設定可
+;;
+;; ;; 検索(全般)
+;; (setq case-fold-search t)
+;;
+;; ;; インクリメンタルサーチ
+;; (setq isearch-case-fold-search nil)
+
+
+;; バッファー名の検索
+(setq read-buffer-completion-ignore-case t)
+
+;; ファイル名の検索
+(setq read-file-name-completion-ignore-case t)
+
+
+;;;置換(全般)
+;; (setq case-replace t)
+
+;; dabbrev 時の置換
+(setq dabbrev-case-replace nil)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; 自動作成ファイル
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;; backup ファイルオープン時のバックアップ (xxx~)
+;; ================================================================
+
+;; 実行の有無
+(setq make-backup-files t)
+
+;; 格納ディレクトリーの変更
+;;   (対象ディレクトリー . 格納ディレクトリー) のリスト
+(setq backup-directory-alist '((".*" . "~/.ehist")))
+
+
+;; 番号付けによる複数保存
+(setq version-control     t)  ;; 実行の有無
+(setq kept-new-versions   5)  ;; 最新の保持数
+(setq kept-old-versions   1)  ;; 最古の保持数
+(setq delete-old-versions t)  ;; 範囲外を削除
+
+
+
+;; auto-save 自動保存ファイル (#xxx#)
+;; ================================================================
+
+;; ;; 実行の有無
+;; (setq auto-save-default nil)
+
+;; ;; 格納ディレクトリーの変更
+;; ;;   (対象ファイルのパターン . 保存ファイルパス) のリスト
+;; (setq auto-save-file-name-transforms
+;;       (append auto-save-file-name-transforms
+;;	      '((".*" "~/tmp/" t))))
+
+
+;; 保存の間隔
+(setq auto-save-timeout 10)	 ;; 秒   (デフォルト : 30)
+(setq auto-save-interval 100)	 ;; 打鍵 (デフォルト : 300)
+
+
+
+
+;; auto-save-list 自動保存のリスト  (~/.emacs.d/auto-save-list/.saves-xxx)
+;; ================================================================
+
+;; 実行の有無
+(setq auto-save-list-file-prefix nil)
+
+;; ;; 格納ディレクトリーの変更
+;; (setq auto-save-list-file-prefix "~/tmp/.saves-")
+
+
+
+;; lock ロックファイル (.#xxx)
+;; ================================================================
+
+;; 実行の有無
+(setq create-lockfiles nil)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; パッケージ
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(when (require 'package nil t)
+  (package-initialize))
+  (setq package-archives
+	(append package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+				   ("marmalade" . "http://marmalade-repo.org/packages/")
+				   ("gnu" . "http://elpa.gnu.org/packages/")
+				   ("org" . "http://orgmode.org/elpa/"))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; auto-complete
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'auto-complete-config)
+(ac-config-default)
+(add-to-list 'ac-modes 'text-mode)         ;; text-modeでも自動的に有効にする
+(add-to-list 'ac-modes 'fundamental-mode)  ;; fundamental-mode
+(add-to-list 'ac-modes 'org-mode)
+(add-to-list 'ac-modes 'yatex-mode)
+(ac-set-trigger-key "TAB")
+(setq ac-use-menu-map t)       ;; 補完メニュー表示時にC-n/C-pで補完候補選択
+(setq ac-use-fuzzy t)          ;; 曖昧マッチ
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; ロードパスの設定
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(add-to-list 'load-path "~/.emacs.d/site-lisp")
+
+;; ;; load-path に登録されたディレクトリーを subdir 扱い
+;; ;;   注:アクセス件でエラーになりやすい
+;; (normal-top-level-add-subdirs-to-load-path)
+
+
+;; ;; キー設定ファイルのロード
+;; (load "my-keyset-light")
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; プログラミング
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; 共通
+;; ================================================================
+
+;; 左端(文字の前)ではインデント、それ以外はタブの挿入
+(setq tab-always-indent nil)
+(setq c-tab-always-indent nil)
+
+;; 空白を一度に削除
+(if (fboundp 'global-hungry-delete-mode)
+    (global-hungry-delete-mode 1))
+
+;; 改行時などに自動でインデント
+;;   (C-j と C-m の入れ替え)
+(if (fboundp 'electric-indent-mode)
+    (electric-indent-mode 0))
+
+;; 特定の文字を入力すると自動で改行、インデント
+;; (electric-layout-mode 1)
+
+
+;; C 系共通
+;; ================================================================
+
+(defun my-all-cc-mode-init ()
+  ;; C 系(cc-mode を継承した)モード共通の設定を記述
+
+  ;; 空白などを一度に削除
+  (c-toggle-hungry-state 1)
+
+  ;; 改行時などで自動インデント
+  ;; (c-toggle-electric-state 1)
+  ;;
+  ;; ";", "}" などを入力したときに自動改行
+  ;; 自動インデントも一緒に ON になる
+  ;; (c-toggle-auto-newline 1)
+
+  )
+(add-hook 'c-mode-common-hook 'my-all-cc-mode-init)
+
+
+;; C, C++
+;; ================================================================
+
+(autoload 'vs-set-c-style "vs-set-c-style"
+  "Set the current buffer's c-style to Visual Studio like style. ")
+
+(defun my-c-c++-mode-init ()
+  ;; C, C++ 用の設定を記述
+
+
+  ;; Visual Studio 風の設定
+  ;; (vs-set-c-style)
+  )
+(add-hook 'c-mode-hook 'my-c-c++-mode-init)
+(add-hook 'c++-mode-hook 'my-c-c++-mode-init)
+
+
+;; .h でも C++
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Tips
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; ビープ音禁止
+(setq ring-bell-function 'ignore)
+
+
+;; スタート画面(メッセージ)を表示しない
+(setq inhibit-startup-screen t)
+
+
+;; 選択領域を削除キーで一括削除
+(delete-selection-mode t)
+
+;; shift + 矢印キーで領域選択
+(if (fboundp 'pc-selection-mode)
+    (pc-selection-mode))
+
+
+;; 行頭 kill-line (C-k) で行全体をカット
+(setq kill-whole-line t)
+
+;; 読み取り専用バッファーでもカット系でコピー可能
+(setq kill-read-only-ok t)
+
+
+;; ediff 時にフレームを使わない
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+
+
+;; png, jpg などのファイルを画像として表示
+(setq auto-image-file-mode t)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; カスタマイズ
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; カスタマイズ用のファイルを設定
+(setq custom-file "~/.emacs.d/custom_setttings.el")
+
+;; カスタマイズ用ファイルをロード
+(load custom-file t)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; 日本語の設定
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; 環境に合わせた日本語文字コードに設定
+;;  Windows => SJIS
+;;  Linux => 環境変数 LANG から
+(require `mozc)
+(set-language-environment "Japanese")
+(prefer-coding-system `utf-8)
+;; set-language-environment でうまくいかない場合に設定
+;; (prefer-coding-system 'utf-8)
+
+;; ファイルの文字コードだけ別に指定したい場合に設定
+;; (setq-default buffer-file-coding-system 'utf-8-with-signature) ; BOM 付き UTF-8
+
+
+
+;; font
+(add-to-list 'default-frame-alist '(font . "ricty-12"))
+
+
+;;
+;; tabbarの設定
+;;
+(require 'tabbar)
+(tabbar-mode)
+
+(tabbar-mwheel-mode nil)                  ;; マウスホイール無効
+(setq tabbar-buffer-groups-function nil)  ;; グループ無効
+(setq tabbar-use-images nil)              ;; 画像を使わない
+
+;;----- キーに割り当てる
+;;(global-set-key (kbd "<C-tab>") 'tabbar-forward-tab)
+;;(global-set-key (kbd "<C-S-tab>") 'tabbar-backward-tab)
+(global-set-key (kbd "<f10>") 'tabbar-forward-tab)
+(global-set-key (kbd "<f9>") 'tabbar-backward-tab)
+(global-set-key (kbd "M-<right>") 'tabbar-forward-tab)
+(global-set-key (kbd "M-<left>") 'tabbar-backward-tab)
+
+;;----- 左側のボタンを消す
+(dolist (btn '(tabbar-buffer-home-button
+	       tabbar-scroll-left-button
+	       tabbar-scroll-right-button))
+  (set btn (cons (cons "" nil)
+		 (cons "" nil))))
+
+
+;;----- タブのセパレーターの長さ
+(setq tabbar-separator '(2.0))
+
+
+;;----- タブの色（CUIの時。GUIの時は後でカラーテーマが適用）
+(set-face-attribute
+ 'tabbar-default nil
+ :background "brightblack"
+ :foreground "white"
+ )
+(set-face-attribute
+ 'tabbar-selected nil
+ :background "#ff5f00"
+ :foreground "brightwhite"
+ :box nil
+ )
+(set-face-attribute
+ 'tabbar-modified nil
+ :background "brightred"
+ :foreground "brightwhite"
+ :box nil
+ )
+
+(when window-system                       ; GUI時
+  ;; 外観変更
+  (set-face-attribute
+   'tabbar-default nil
+   :family "MeiryoKe_Gothic"
+   :background "#34495E"
+   :foreground "#EEEEEE"
+   :height 0.85
+   )
+  (set-face-attribute
+   'tabbar-unselected nil
+   :background "#34495E"
+   :foreground "#EEEEEE"
+   :box nil
+  )
+  (set-face-attribute
+   'tabbar-modified nil
+   :background "#E67E22"
+   :foreground "#EEEEEE"
+   :box nil
+  )
+  (set-face-attribute
+   'tabbar-selected nil
+   :background "#E74C3C"
+   :foreground "#EEEEEE"
+   :box nil)
+  (set-face-attribute
+   'tabbar-button nil
+   :box nil)
+  (set-face-attribute
+   'tabbar-separator nil
+   :height 2.0)
+)
+
+;;----- 表示するバッファ
+(defun my-tabbar-buffer-list ()
+  (delq nil
+	(mapcar #'(lambda (b)
+		    (cond
+		     ;; Always include the current buffer.
+		     ((eq (current-buffer) b) b)
+		     ((buffer-file-name b) b)
+		     ((char-equal ?\  (aref (buffer-name b) 0)) nil)
+;                     ((equal "*scratch*" (buffer-name b)) b) ; *scratch*バッファは表示する
+		     ((char-equal ?* (aref (buffer-name b) 0)) nil) ; それ以外の * で始まるバッファは表示しない
+		     ((buffer-live-p b) b)))
+		(buffer-list))))
+(setq tabbar-buffer-list-function 'my-tabbar-buffer-list)
+(put 'dired-find-alternate-file 'disabled nil)
+
+;; タイトルパーにファイルのフルパスを表示する
+(setq frame-title-format "%f")
+
+;; 行番号を表示する
+(global-linum-mode t)
+
+;; カラム番号も表示する
+(column-number-mode t)
